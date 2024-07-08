06f38b38d6811038ca9ffca75485a583bcc95203
Ari Wahidin <ari.wahidin@PYIDJKT.local>
2024-07-08 13:13:11 +0700

    first commit

--- app.js
+++ app.js
@@ -15,7 +15,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 console.log(__dirname)
-app.use(express.static(path.join(__dirname, 'public')));
+// app.use(express.static(path.join(__dirname, 'public')));
+app.use(baseUrl, express.static(path.join(__dirname, 'public')));
 
 app.set('view engine', 'ejs');
 
