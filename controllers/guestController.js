bf9d867c9914da44da7f36e38a848a9716c99229
Ari Wahidin <ari.wahidin@PYIDJKT.local>
2024-07-10 11:29:29 +0700

    add update status order

--- controllers/guestController.js
+++ controllers/guestController.js
@@ -58,10 +58,72 @@ exports.getOrderBySPK = async (req, res) => {
         WHERE a.order_id = ?
         GROUP BY a.ship_to`;
 
-
-
-
     } catch (error) {
 
     }
 };
+
+exports.getOrderStatus = async (req, res) => {
+    console.log(req.body);
+    const { order_id, ship_to } = req.body;
+    const sql = `SELECT * FROM order_d_status WHERE order_id = ? AND ship_to = ?`;
+
+    try {
+        const [rows] = await db.execute(sql, [order_id, ship_to]);
+        res.status(200).json({ success: true, data: rows });
+    } catch (error) {
+        console.error(error);
+        return res.status(500).send('Internal Server Error');
+    }
+}
+
+exports.saveOrderStatus = async (req, res) => {
+    const { spk, ship_to, delivery_no, lokasi_terkini, status, user_lat, user_lon, user_address, user_name } = req.body;
+    const date = new Date().toISOString().slice(0, 19).replace('T', ' ');
+    // const db = getDbConnection(spk);
+
+    // if (!db) {
+    //     return res.status(400).json({ success: false, message: 'Invalid SPK provided' });
+    // }
+
+    const where = {
+        order_id: spk,
+        ship_to: ship_to,
+        order_status: 'goods arrived'
+    };
+
+    try {
+        const checkQuery = `SELECT * FROM order_d_status WHERE order_id = ? AND ship_to = ? AND order_status = 'goods arrived'`;
+        const [rows] = await db.execute(checkQuery, [spk, ship_to]);
+
+        if (rows.length < 1) {
+            const query = `
+                INSERT INTO order_d_status (
+                    order_id, ship_to, delivery_no, lokasi_terkini, 
+                    order_status, lat, lon, address, created_date, created_by
+                ) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
+            `;
+
+            const values = [
+                spk, ship_to, delivery_no, lokasi_terkini,
+                status, user_lat, user_lon, user_address,
+                date, user_name
+            ];
+
+            try {
+                const [results] = await db.execute(query, values);
+                res.json({ success: true, message: 'Order status updated successfully' });
+            } catch (err) {
+                console.error('Error executing query:', err);
+                res.status(500).json({ success: false, message: 'Error executing query' });
+            }
+            return;
+        }
+
+
+        res.status(200).json({ success: false, messages: 'Barang sampai sudah pernah di isi' });
+    } catch (error) {
+        console.error(error);
+        return res.status(500).send('Internal Server Error');
+    }
+}
--- routes/guestRoutes.js
+++ routes/guestRoutes.js
@@ -4,6 +4,8 @@ const {optionalAuthenticateToken} = require('../middleware/authenticateJWT');
 const guestController = require('../controllers/guestController');
 
 router.get('/order',optionalAuthenticateToken, guestController.getOrder);
+router.post('/order/status',optionalAuthenticateToken, guestController.getOrderStatus);
+router.post('/order/save',optionalAuthenticateToken, guestController.saveOrderStatus);
 router.post('/getOrder', guestController.getOrderBySPK);
 
 module.exports = router;
