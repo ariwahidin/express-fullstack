2d571943a37b8e6cb3cf37f4c1a9ab9635a76791
Ari Wahidin <ari.wahidin@PYIDJKT.local>
2024-08-23 09:22:29 +0700

    fix maxAge token

--- controllers/authController.js
+++ controllers/authController.js
@@ -28,6 +28,7 @@ exports.login = async (req, res) => {
 
             res.cookie('token', token, {
                 httpOnly: true,
+                secure: true,
                 maxAge: 7 * 24 * 60 * 60 * 1000 // 7 days in milliseconds
             });
             
